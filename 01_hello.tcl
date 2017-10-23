0631e62e5bba1d776c4a13de0240d967cc3534f0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-23 21:24:48 +0700

    Add various comments

--- 01_hello.tcl
+++ 01_hello.tcl
@@ -1,4 +1,11 @@
 #!/usr/bin/tclsh
 
-puts "Hello world from ffr"
+# This a comment
+puts "Hello world from ffr" ;# and inline comment, ; is strictly needed
+
+# Multiline comment
+if 0 {
+  This is multiline comment.
+  It is quite awkward IMHO.
+}
 
